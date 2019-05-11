e271b4ef0488307a3873a3fc530857ae2156d64d
DandiBaroes <dandibaroes88@gmail.com>
2019-05-12 00:44:26 +0700

    menambahkan logika session pada page_header

--- application/views/page_header.php
+++ application/views/page_header.php
@@ -45,9 +45,19 @@
           <li class="nav-item mr-3" >
             <a class="nav-link text-white" href="#">About</a>
           </li>
-          <li class="nav-item mr-3" >
-            <a class="nav-link text-white" href="#">Logout</a>
-          </li>
+          <?php
+          if (($this->session->userdata('email'))!=null){
+                        echo "
+                        <li class='nav-item mr-3' >
+                        <a class='nav-link text-white' href=".base_url('Cuser/logPage/').">Logout</a>
+                        </li>
+                        ";
+                    }else{
+                        echo "
+                        ";
+                        }
+                        ?>
+          
       </ul>
     </nav>
     <div id="mySidenav" class="sidenav">
