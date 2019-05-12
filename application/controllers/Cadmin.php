e6757ede542aae4470239f8764837f9f5378f221
DandiBaroes <dandibaroes88@gmail.com>
2019-05-12 11:59:12 +0700

    add Controller admin and M_admin

---
+++ application/controllers/Cadmin.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cadmin extends CI_Controller {
+
+	public function __construct()
+ 	{
+ 		parent::__construct();
+ 		$this->load->model('M_admin');
+    }
+
+    public function index()
+	{
+		$this->load->view('page_awal');
+    }
+
+    public function logPage()
+	{
+		$this->load->view('user/page_login');
+    }
+
+      public function signIn(){
+        $user = $this->M_admin->findUser();
+        if($user != FALSE){
+          $this->session->set_userdata('username',$user['username']);
+          redirect(site_url('Cadmin/index'));
+        }else{
+          redirect(site_url('Cadmin/logPage'));
+        }
+      }
+
+      public function logOut(){
+        $this->session->sess_destroy();
+        redirect(base_url(''));
+    }
+
+    }
