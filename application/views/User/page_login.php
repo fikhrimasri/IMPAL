2b4b5537433688b5cbc7a82b9b2a8b5f61a1fbb0
DandiBaroes <dandibaroes88@gmail.com>
2019-05-12 23:52:28 +0700

    remove forgot password

--- application/views/User/page_login.php
+++ application/views/User/page_login.php
@@ -52,10 +52,10 @@
                         <input name ="username" type="text" placeholder="Username..." class="form-control mb-2"required>
                         <label for="Password"style="color:white"><h6>Password</h6></label>
                         <input name="password" type="password" placeholder="Password..." class="form-control mb-2"required>
-                        <a href="#"><h6 style="color:white">Lupa Password?</h6></a>
+                        <br>
                         <div class="cekbox mt-2">
                               <label class="checkbox pull-left" for="checkbox">
-                                    <input type="checkbox"><span style="color:white">Remember me</span>
+                                    <input type="checkbox"><span style="color:black"> remember me</span>
                                  </label>
                         </div>
                         <div class="text-center">
@@ -84,11 +84,11 @@
                               <input name ="username" type="text" class="form-control mb-2" placeholder="Username..." required>
                               <label for="Password"style="color:white"><h6>Password</h6></label>
                               <input name="password" type="password" class="form-control mb-2" placeholder="Password..." required>
-                              <a href="#" style="color:white"><h6>Lupa Password?</h6></a>
+                              <br>
                               <div class="cekbox mt-2">
                                  <label for="checkbox" class="checkbox pull-left">
                                        <input type="checkbox">
-                                       <span style="color:white">Remember me</span>
+                                       <span style="color:black"> remember me</span>
                                  </label>
                                  
                               </div>
