9825b22de942e390cf7715e4b1178400a5c7eff1
Fikhri <36291552+fikhrimasri@users.noreply.github.com>
2019-05-13 16:22:52 +0700

    fixed title

--- application/views/page_header_admin.php
+++ application/views/page_header_admin.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>GangBank</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
