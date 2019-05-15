e3e8baa1ecda663b11694d386d6c09361e7b937e
khakans <khairul-kanters@outlook.com>
2019-05-15 13:24:47 +0700

    tabel fix

--- application/views/User/page_HistoryTransaksi.php
+++ application/views/User/page_HistoryTransaksi.php
@@ -45,11 +45,4 @@
   overflow-y: auto;
   width: 100%;
 }
-.table-fixed thead, .table-fixed tbody, .table-fixed tr, .table-fixed td, .table-fixed th {
-  display: block;
-}
-.table-fixed tbody td, .table-fixed thead > tr> th {
-  float: left;
-  border-bottom-width: 0;
-}
 </style>
